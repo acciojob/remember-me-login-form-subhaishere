9b57cb419192e8fc93d5cafbca231f5864929031
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-10-27 17:35:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 //your JS code here. If required.
-let formEle = document.querySelector("form");
+//your JS code here. If required.
+window.onload = function() {
+    // check if there are saved details
+    if(localStorage.getItem('username') && localStorage.getItem('password')) {
+        // show button to login as existing user
+        var btn = document.getElementById('existing');
+        btn.style.display = "block";
+        btn.onclick = function() {
+            alert('Logged in as ' + localStorage.getItem('username'));
+        };
+        // document.getElementById('form').appendChild(btn);
+    }
+};
 
-formEle.addEventListener("submit", submitForm);
+function submitForm() {
+    // get form data
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var remember = document.getElementById('checkbox').checked;
 
-function submitForm(e){
-	e.preventDefault();
-	let checkEle = document.getElementById("checkbox").checked;
-	alert( `${checkEle}`);
+    // save details to localstorage if checkbox is checked
+    if(remember) {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+    } else {
+        // remove saved details from localstorage if checkbox is unchecked
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+    }
+
+    // show alert with username
+    alert('Logged in as ' + username);
 }
