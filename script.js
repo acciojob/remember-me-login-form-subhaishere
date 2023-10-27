9bc0287a5a1ec873150740fda049715ea32e90c0
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-10-27 17:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+const check=document.getElementById("checkbox");
+const submit=document.getElementById("submit");
+const form=document.getElementById("form");
+
+form.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  const name1=document.getElementById("username").value;
+const pass=document.getElementById("password").value;
+ let userData=[name1, pass]
+  if(check.checked == true){
+   
+    localStorage.setItem("user", JSON.stringify(userData))
+  }
+  else{
+  
+    localStorage.clear("user");
+  }
+})
